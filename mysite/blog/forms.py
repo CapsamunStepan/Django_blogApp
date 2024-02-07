241b214b367b8994941206511f3a42bfa0206052
Steve <stepan.capsamun@isa.utm.md>
2024-02-07 13:18:48 +0200

    hide sender email and label

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -4,7 +4,7 @@ from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
-    email = forms.EmailField()
+    email_ = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
 
